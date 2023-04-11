802b70e7f143044beb32587be47c86e08ddd16f0
ariso-sac <86800159+ariso-sac@users.noreply.github.com>
2023-04-11 23:34:45 +0530

    Update test.py
    With proper user testing

--- Milestone-5/test.py
+++ Milestone-5/test.py
@@ -1,6 +1,8 @@
 import pytest
 import requests
 
+# User CRUD Opertations 
+
 def test_create_user():
     url = "http://0.0.0.0:8080/api/client"
 
@@ -9,7 +11,6 @@ def test_create_user():
         "password": "password",
         "role": "Student"
     }
-
 
     response = requests.post(url, data=payload)
 
@@ -21,36 +22,31 @@ def test_create_user():
     assert response.json()["password"] == "password"
     assert response.json()['role_details']["role_name"] == "Student"
 
-
 def test_get_user_all():
     url = "http://0.0.0.0:8080/api/client"
 
     response = requests.get(url)
+    print(response.text)
 
     # Assert that the request-response cycle completed successfully.
     assert response.status_code == 200
-
 
 def test_get_user_unique():
     url = "http://0.0.0.0:8080/api/client/1"
 
     response = requests.get(url)
+    print(response.text)
+
 
     # Assert that the request-response cycle completed successfully.
     assert response.status_code == 200
 
-    # Assert that the response body is what we expect.
-    assert response.json()["email"] == "test@example.com"
-    assert response.json()["password"] == "password"
-    assert response.json()["role_name"] == "Student"
-
-
 def test_put_user():
-    url = "http://0.0.0.0:8080/api/client"
+    url = "http://0.0.0.0:8080/api/client/1"
 
     payload = {
         "id": "1",
-        "email": "test@example.com",
+        "email": "updated@example.com",
         "password": "newpassword",
         "role": "Student"
     }
@@ -62,9 +58,19 @@ def test_put_user():
     assert response.status_code == 200
 
     # Assert that the response body is what we expect.
-    assert response.json()["email"] == "test@example.com"
-    assert response.json()["password"] == "newpassword"
-    assert response.json()["role_name"] == "Student"
+    assert response.json()["email"] == "updated@example.com"
+
+def test_delete_user():
+    url = "http://0.0.0.0:8080/api/client/1"
+
+    response = requests.delete(url)
+    print(response.text)
+
+
+    # Assert that the request-response cycle completed successfully.
+    assert response.status_code == 200
+
+# Testing Query and FAQ 
 
 def create_query():
     url = "http://0.0.0.0:8080/api/query"
@@ -100,6 +106,7 @@ def create_faq():
 
 
     response = requests.post(url, data=payload)
+    print(response.text)
 
     # Assert that the request-response cycle completed successfully.
     assert response.status_code == 201
@@ -107,3 +114,7 @@ def create_faq():
     # # Assert that the response body is what we expect.
     assert response.json()["issue"] == "Test Issue"
     assert response.json()["solution"] == "No Solution"
+
+
+# if __name__ == "__main__":
+#     create_faq()
